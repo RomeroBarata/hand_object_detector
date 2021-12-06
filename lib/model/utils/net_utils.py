18b77ec17ddd901f37cf2eda0beec50fbec65777
RomeroBarata <romero.barata@gmail.com>
2021-12-06 12:45:03 +1100

    Aesthetic changes.

--- lib/model/utils/net_utils.py
+++ lib/model/utils/net_utils.py
@@ -11,11 +11,13 @@ import random
 from PIL import Image, ImageDraw, ImageFont
 from model.utils.viz_hand_obj import *
 
+
 def save_net(fname, net):
     import h5py
     h5f = h5py.File(fname, mode='w')
     for k, v in net.state_dict().items():
         h5f.create_dataset(k, data=v.cpu().numpy())
+
 
 def load_net(fname, net):
     import h5py
@@ -23,6 +25,7 @@ def load_net(fname, net):
     for k, v in net.state_dict().items():
         param = torch.from_numpy(np.asarray(h5f[k]))
         v.copy_(param)
+
 
 def weights_normal_init(model, dev=0.01):
     if isinstance(model, list):
@@ -49,6 +52,7 @@ def clip_gradient(model, clip_norm):
         if p.requires_grad and p.grad is not None:
             p.grad.mul_(norm)
 
+
 def vis_detections(im, class_name, dets, thresh=0.8):
     """Visual debugging of detections."""
     for i in range(np.minimum(10, dets.shape[0])):
@@ -66,6 +70,7 @@ def vis_detections(im, class_name, dets, thresh=0.8):
                 cv2.putText(im, '%s: %.3f' % (class_name, score), (bbox[0], bbox[1] + 15), cv2.FONT_HERSHEY_PLAIN,
                             1.0, (0, 0, 255), thickness=1)
     return im
+
 
 def vis_detections_filtered_objects(im, obj_dets, hand_dets, thresh=0.8):
     """Visual debugging of detections."""
@@ -95,15 +100,14 @@ def vis_detections_filtered_objects(im, obj_dets, hand_dets, thresh=0.8):
     return im
 
 
-
-def vis_detections_filtered_objects_PIL(im, obj_dets, hand_dets, thresh_hand=0.8, thresh_obj=0.01, font_path='lib/model/utils/times_b.ttf'):
-
+def vis_detections_filtered_objects_PIL(im, obj_dets, hand_dets,
+                                        thresh_hand=0.8, thresh_obj=0.01, font_path='lib/model/utils/times_b.ttf'):
     # convert to PIL
-    im = im[:,:,::-1]
+    im = im[:, :, ::-1]
     image = Image.fromarray(im).convert("RGBA")
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(font_path, size=30)
-    width, height = image.size 
+    width, height = image.size
 
     if (obj_dets is not None) and (hand_dets is not None):
         img_obj_id = filter_object(obj_dets, hand_dets)
@@ -122,33 +126,28 @@ def vis_detections_filtered_objects_PIL(im, obj_dets, hand_dets, thresh_hand=0.8
             if score > thresh_hand:
                 # viz hand by PIL
                 image = draw_hand_mask(image, draw, hand_idx, bbox, score, lr, state, width, height, font)
-
-                if state > 0: # in contact hand
-
-                    obj_cc, hand_cc =  calculate_center(obj_dets[img_obj_id[i],:4]), calculate_center(bbox)
+                if state > 0:  # in contact hand
+                    obj_cc, hand_cc = calculate_center(obj_dets[img_obj_id[i], :4]), calculate_center(bbox)
                     # viz line by PIL
                     if lr == 0:
                         side_idx = 0
                     elif lr == 1:
                         side_idx = 1
-                    draw_line_point(draw, side_idx, (int(hand_cc[0]), int(hand_cc[1])), (int(obj_cc[0]), int(obj_cc[1])))
-
-
-        
-
+                    draw_line_point(draw, side_idx,
+                                    (int(hand_cc[0]), int(hand_cc[1])), (int(obj_cc[0]), int(obj_cc[1])))
     elif hand_dets is not None:
         image = vis_detections_PIL(im, 'hand', hand_dets, thresh_hand, font_path)
-        
     return image
+
 
 def vis_detections_PIL(im, class_name, dets, thresh=0.8, font_path='lib/model/utils/times_b.ttf'):
     """Visual debugging of detections."""
-    
+
     image = Image.fromarray(im).convert("RGBA")
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(font_path, size=30)
     width, height = image.size
-    
+
     for hand_idx, i in enumerate(range(np.minimum(10, dets.shape[0]))):
         bbox = list(int(np.round(x)) for x in dets[i, :4])
         score = dets[i, 4]
@@ -156,13 +155,13 @@ def vis_detections_PIL(im, class_name, dets, thresh=0.8, font_path='lib/model/ut
         state = dets[i, 5]
         if score > thresh:
             image = draw_hand_mask(image, draw, hand_idx, bbox, score, lr, state, width, height, font)
-            
-    return image
 
+    return image
 
 
 def calculate_center(bb):
     return [(bb[0] + bb[2])/2, (bb[1] + bb[3])/2]
+
 
 def filter_object(obj_dets, hand_dets):
     filtered_object = []
@@ -182,6 +181,7 @@ def filter_object(obj_dets, hand_dets):
         img_obj_id.append(dist_min)
     return img_obj_id
 
+
 def adjust_learning_rate(optimizer, decay=0.1):
     """Sets the learning rate to the initial LR decayed by 0.5 every 20 epochs"""
     for param_group in optimizer.param_groups:
@@ -190,6 +190,7 @@ def adjust_learning_rate(optimizer, decay=0.1):
 
 def save_checkpoint(state, filename):
     torch.save(state, filename)
+
 
 def _smooth_l1_loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights, sigma=1.0, dim=[1]):
 
@@ -203,9 +204,10 @@ def _smooth_l1_loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_w
     out_loss_box = bbox_outside_weights * in_loss_box
     loss_box = out_loss_box
     for i in sorted(dim, reverse=True):
-      loss_box = loss_box.sum(i)
+        loss_box = loss_box.sum(i)
     loss_box = loss_box.mean()
     return loss_box
+
 
 def _crop_pool_layer(bottom, rois, max_pool=True):
     # code modified from
@@ -238,28 +240,29 @@ def _crop_pool_layer(bottom, rois, max_pool=True):
 
     # affine theta
     zero = Variable(rois.data.new(rois.size(0), 1).zero_())
-    theta = torch.cat([\
-      (x2 - x1) / (width - 1),
-      zero,
-      (x1 + x2 - width + 1) / (width - 1),
-      zero,
-      (y2 - y1) / (height - 1),
-      (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
+    theta = torch.cat([ \
+        (x2 - x1) / (width - 1),
+        zero,
+        (x1 + x2 - width + 1) / (width - 1),
+        zero,
+        (y2 - y1) / (height - 1),
+        (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
 
     if max_pool:
-      pre_pool_size = cfg.POOLING_SIZE * 2
-      grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, pre_pool_size, pre_pool_size)))
-      bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W)\
-                                                                .contiguous().view(-1, D, H, W)
-      crops = F.grid_sample(bottom, grid)
-      crops = F.max_pool2d(crops, 2, 2)
+        pre_pool_size = cfg.POOLING_SIZE * 2
+        grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, pre_pool_size, pre_pool_size)))
+        bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W) \
+            .contiguous().view(-1, D, H, W)
+        crops = F.grid_sample(bottom, grid)
+        crops = F.max_pool2d(crops, 2, 2)
     else:
-      grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, cfg.POOLING_SIZE, cfg.POOLING_SIZE)))
-      bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W)\
-                                                                .contiguous().view(-1, D, H, W)
-      crops = F.grid_sample(bottom, grid)
+        grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, cfg.POOLING_SIZE, cfg.POOLING_SIZE)))
+        bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W) \
+            .contiguous().view(-1, D, H, W)
+        crops = F.grid_sample(bottom, grid)
 
     return crops, grid
+
 
 def _affine_grid_gen(rois, input_size, grid_size):
 
@@ -273,17 +276,18 @@ def _affine_grid_gen(rois, input_size, grid_size):
     width = input_size[1]
 
     zero = Variable(rois.data.new(rois.size(0), 1).zero_())
-    theta = torch.cat([\
-      (x2 - x1) / (width - 1),
-      zero,
-      (x1 + x2 - width + 1) / (width - 1),
-      zero,
-      (y2 - y1) / (height - 1),
-      (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
+    theta = torch.cat([ \
+        (x2 - x1) / (width - 1),
+        zero,
+        (x1 + x2 - width + 1) / (width - 1),
+        zero,
+        (y2 - y1) / (height - 1),
+        (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
 
     grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, grid_size, grid_size)))
 
     return grid
+
 
 def _affine_theta(rois, input_size):
 
@@ -306,12 +310,12 @@ def _affine_theta(rois, input_size):
     #   (y2 - y1) / (height - 1),
     #   (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
 
-    theta = torch.cat([\
-      (y2 - y1) / (height - 1),
-      zero,
-      (y1 + y2 - height + 1) / (height - 1),
-      zero,
-      (x2 - x1) / (width - 1),
-      (x1 + x2 - width + 1) / (width - 1)], 1).view(-1, 2, 3)
+    theta = torch.cat([ \
+        (y2 - y1) / (height - 1),
+        zero,
+        (y1 + y2 - height + 1) / (height - 1),
+        zero,
+        (x2 - x1) / (width - 1),
+        (x1 + x2 - width + 1) / (width - 1)], 1).view(-1, 2, 3)
 
     return theta
